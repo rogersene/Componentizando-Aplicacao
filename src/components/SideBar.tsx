eb35826cf549f1cff2820fccf2fbb58b9adcbe6e
Roger Crescencio <rogersenefaria@gmail.com>
2022-09-30 15:25:17 -0300

    refactor: performed componentization of the sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: any;
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SideBarProps ) {
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
